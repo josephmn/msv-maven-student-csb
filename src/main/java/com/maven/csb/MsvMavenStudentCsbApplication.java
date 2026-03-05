0255c1e6e6ba481d0a52ac14cf833d6c0b9231a0
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2026-03-05 15:21:38 -0500

    first commit

---
+++ src/main/java/com/maven/csb/MsvMavenStudentCsbApplication.java
@@ -0,0 +1,13 @@
+package com.maven.csb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvMavenStudentCsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvMavenStudentCsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/csb/MsvMavenStudentCsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.csb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvMavenStudentCsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
